32f767c01307c07ce383bfa4036333b0b3671b4c
Dreamer206602 <boobooMX@163.com>
2017-03-18 22:24:35 +0800

    调整首页布局

--- app/src/main/java/com/booboomx/hotvideo/ui/activity/MainActivity.java
+++ app/src/main/java/com/booboomx/hotvideo/ui/activity/MainActivity.java
@@ -1,15 +1,44 @@
 package com.booboomx.hotvideo.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
 
+import com.afollestad.materialdialogs.color.ColorChooserDialog;
 import com.booboomx.hotvideo.R;
+import com.booboomx.hotvideo.base.BaseActivity;
+import com.booboomx.hotvideo.ui.view.MainView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * 主界面
+ */
+public class MainActivity extends BaseActivity implements ColorChooserDialog.ColorCallback{
+
+
+    @BindView(R.id.main_view)
+    MainView mMainView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mUnBinder= ButterKnife.bind(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+    }
+
+    @Override
+    public void onColorSelection(@NonNull ColorChooserDialog dialog, @ColorInt int selectedColor) {
+
     }
 }
